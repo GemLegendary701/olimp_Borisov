d7616b72be4782e65c883ca36ae3636c274f29b9
GemLegendary701 <demon.8025@mail.ru>
2025-11-07 11:42:48 +0500

    Add files via upload

---
+++ Borisov_D_PR_33_zd_1/Borisov_D_PR_33_zd_1/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] parts = File.ReadAllText("input.txt").Split();
+int k = int.Parse(parts[0]);
+int n = int.Parse(parts[1]);
+int res = n / k;
+File.WriteAllText("output.txt", res.ToString());
