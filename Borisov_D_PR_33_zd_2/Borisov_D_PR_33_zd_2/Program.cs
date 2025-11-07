8adac1ce7acd0f5c1e4ce6ec49634890c4fa84f8
GemLegendary701 <demon.8025@mail.ru>
2025-11-07 11:43:32 +0500

    Add files via upload

---
+++ Borisov_D_PR_33_zd_2/Borisov_D_PR_33_zd_2/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int n = int.Parse(lines[0]);
+using (StreamWriter wr = new StreamWriter("output.txt"))
+{
+    for (int i = 1; i <= n; i++)
+    {
+        bool isPalind = true;
+        string word = lines[i];
+        int right = word.Length - 1;
+        int left = 0;
+        while (left < right)
+        {
+            if (char.ToUpper(word[left]) != char.ToUpper(word[right]))
+            {
+                isPalind = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        wr.WriteLine(isPalind ? "YES" : "NO");
+    }
+}
