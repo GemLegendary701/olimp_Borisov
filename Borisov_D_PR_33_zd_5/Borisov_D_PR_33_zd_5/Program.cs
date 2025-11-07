b9c35b281809834009ce96936ea14d0ee8632539
GemLegendary701 <demon.8025@mail.ru>
2025-11-07 11:46:30 +0500

    Add files via upload

---
+++ Borisov_D_PR_33_zd_5/Borisov_D_PR_33_zd_5/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int n = int.Parse(lines[0]);
+string[] speedTime = lines[1].Split(' ');
+int v = int.Parse(speedTime[0]);
+int t = int.Parse(speedTime[1]);
+int rast = v * t;
+int k = rast % n;
+File.WriteAllText("output.txt", k.ToString());
